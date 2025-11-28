14dd6105170d88f3fcb747dc0f6b3305279e615a
Sahana Narasipura Vasudevarao <nvsahanarao@gmail.com>
2025-11-27 17:35:55 -0800

    updated to render

---
+++ backend/render-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python3 -m prisma generate
+
+# Copy Prisma binary from build cache to runtime directory
+PRISMA_CACHE_SRC="$XDG_CACHE_HOME/prisma-python/binaries"
+PRISMA_CACHE_DEST="/opt/render/project/src/prisma-binaries"
+
+if [ -d "$PRISMA_CACHE_SRC" ]; then
+    echo "Copying Prisma binaries from $PRISMA_CACHE_SRC to $PRISMA_CACHE_DEST"
+    mkdir -p "$PRISMA_CACHE_DEST"
+    cp -R "$PRISMA_CACHE_SRC"/* "$PRISMA_CACHE_DEST"
+fi
